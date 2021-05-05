c32624c0e1065fc7779da7cfeae7fc36d414d474
Nam Trung <lctrungnam@gmail.com>
2021-05-05 19:42:48 +0700

    first commit

---
+++ Project/PhatTrienUngDungWeb/ShopGiay/App_Start/RouteConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ShopGiay
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+             name: "Register",
+             url: "register",
+             defaults: new { controller = "Register", action = "Index", id = UrlParameter.Optional }
+         );
+            routes.MapRoute(
+             name: "Single",
+             url: "single",
+             defaults: new { controller = "Single", action = "Index", id = UrlParameter.Optional }
+         );
+            routes.MapRoute(
+              name: "Login",
+              url: "login",
+              defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+               name: "GioHang",
+               url: "gio-hang",
+               defaults: new { controller = "GioHang", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "SanPhamShop",
+               url: "san-pham-shop",
+               defaults: new { controller = "SanPhamShop", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "LienHe",
+               url: "lien-he",
+               defaults: new { controller = "LienHe", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Site", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Project/PhatTrienUngDungWeb/ShopGiay/Controllers/SanPhamShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopGiay.Controllers
+{
+    public class SanPhamShopController : Controller
+    {
+        // GET: SanPhamShop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
